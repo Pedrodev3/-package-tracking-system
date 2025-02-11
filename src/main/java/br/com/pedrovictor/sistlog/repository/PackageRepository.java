ab0743a3d38195dadf69d83cfb81bd8d934bd7a2
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-11 18:41:56 -0300

    Feat: Created PackageRepository.java

---
+++ src/main/java/br/com/pedrovictor/sistlog/repository/PackageRepository.java
@@ -0,0 +1,11 @@
+package br.com.pedrovictor.sistlog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PackageRepository extends JpaRepository<Package, Long> {
+
+}
