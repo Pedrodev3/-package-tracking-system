35ccce1793ba8d5970e9f7876e6c28afaf6fba3a
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-10 21:52:31 -0300

    First commit

---
+++ src/main/java/br/com/pedrovictor/sistlog/SistLogApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedrovictor.sistlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pedrovictor/sistlog/TestSistLogApplication.java
@@ -0,0 +1,11 @@
+package br.com.pedrovictor.sistlog;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSistLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(SistLogApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
