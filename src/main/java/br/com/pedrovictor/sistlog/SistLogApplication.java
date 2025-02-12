06ad722c75431aa0ed03b879aebcb7ed5cca90df
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-12 17:31:43 -0300

    Feat: Inserting annotation "@EnableAsync" - Let Open Feign and @Async Operations work together.

--- src/main/java/br/com/pedrovictor/sistlog/SistLogApplication.java
+++ src/main/java/br/com/pedrovictor/sistlog/SistLogApplication.java
@@ -3,9 +3,11 @@ package br.com.pedrovictor.sistlog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "br.com.pedrovictor.sistlog.external")
+@EnableAsync
 public class SistLogApplication {
 
     public static void main(String[] args) {
