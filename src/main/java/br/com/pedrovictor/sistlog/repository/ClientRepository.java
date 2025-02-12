a45e3ebc7fedd74cfacd42d54cb9909f7ee6386d
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-12 17:28:46 -0300

    Feat: Created repositories for the insertion of Recipient and Sender into their respective tables.

--- src/main/java/br/com/pedrovictor/sistlog/repository/ClientRepository.java
+++ src/main/java/br/com/pedrovictor/sistlog/repository/ClientRepository.java
@@ -2,10 +2,11 @@ package br.com.pedrovictor.sistlog.repository;
 
 import br.com.pedrovictor.sistlog.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findClientById(Long id);
     Optional<Client> findByRecipient(String client);
 }
--- src/main/java/br/com/pedrovictor/sistlog/repository/SenderRepository.java
+++ src/main/java/br/com/pedrovictor/sistlog/repository/SenderRepository.java
@@ -2,9 +2,11 @@ package br.com.pedrovictor.sistlog.repository;
 
 import br.com.pedrovictor.sistlog.domain.Sender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SenderRepository extends JpaRepository<Sender, Long> {
     Optional<Sender> findSenderById(Long id);
     Optional<Sender> findBySender(String sender);
