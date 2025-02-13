d53a958dc509b374838dfaa2b89909749820b35a
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-13 13:11:13 -0300

    Feat: Implemented findByRecipientsAndOrSenders() in PackRepository
    Added a join with T_SENDERS and T_CLIENTS to retrieve all packages where the recipient is present in the provided lists.
    If recipients or senders are null, the query returns all rows from T_PACKAGES from "T_PACKAGES".

--- src/main/java/br/com/pedrovictor/sistlog/repository/PackRepository.java
+++ src/main/java/br/com/pedrovictor/sistlog/repository/PackRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,4 +23,11 @@ public interface PackRepository extends JpaRepository<Pack, Long> {
             "LEFT JOIN FETCH p.events " +
             "WHERE p.id = :id")
     Optional<Pack> findByIdWithEvents(@Param("id") Long id);
+
+    @Query("SELECT p FROM Pack p " +
+            "LEFT JOIN FETCH p.sender " +
+            "LEFT JOIN FETCH p.recipient " +
+            "WHERE (:recipients IS NULL OR p.recipient.recipient IN :recipients) " +
+            "AND  (:senders IS NULL OR p.sender.sender IN :senders)")
+    List<Pack> findByRecipientsAndOrSenders(List<String> recipients, List<String> senders);
 }
