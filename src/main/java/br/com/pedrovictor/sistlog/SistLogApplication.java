aab43ea442f2f78dd5bd865837fedf68286bb434
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-13 02:18:51 -0300

    Feat: Configuring AsyncConfig and Retry - For: Avoid Dead Lock and starvation

--- src/main/java/br/com/pedrovictor/sistlog/SistLogApplication.java
+++ src/main/java/br/com/pedrovictor/sistlog/SistLogApplication.java
@@ -3,11 +3,13 @@ package br.com.pedrovictor.sistlog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "br.com.pedrovictor.sistlog.external")
 @EnableAsync
+@EnableRetry
 public class SistLogApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/br/com/pedrovictor/sistlog/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package br.com.pedrovictor.sistlog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class AsyncConfig {
+
+    public Executor trackingEventExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // Número mínimo de threads disponíveis
+        executor.setMaxPoolSize(10); // Número máximo de threads simultâneas
+        executor.setQueueCapacity(50); // Número máximo de tarefas aguardando execução
+        executor.setThreadNamePrefix("TrackingEventThread-");
+        executor.initialize();
+
+        executor.setQueueCapacity(20); // Mantém um número razoável de tarefas em espera
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // Evita descarte de tarefas
+        return executor;
+    }
+}
