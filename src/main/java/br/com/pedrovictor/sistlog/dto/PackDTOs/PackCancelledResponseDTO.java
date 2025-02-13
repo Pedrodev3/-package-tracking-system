d6660d38d669aec980fdb1eb4efcaf8a09c0508b
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-13 17:00:56 -0300

    Feat: Implemented PackCancelledResponseDTO.

---
+++ src/main/java/br/com/pedrovictor/sistlog/dto/PackDTOs/PackCancelledResponseDTO.java
@@ -0,0 +1,19 @@
+package br.com.pedrovictor.sistlog.dto.PackDTOs;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PackCancelledResponseDTO {
+    private Long id;
+    private String status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+    private LocalDateTime dataAtualizacao;
+}
