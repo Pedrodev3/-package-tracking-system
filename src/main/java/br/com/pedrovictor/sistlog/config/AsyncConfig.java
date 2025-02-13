f842e634119d875cf574b2dba12e557c4df9a0f4
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-13 16:55:59 -0300

    Fix: Declared Bean for trackingEventExecutor - Resolved a Spring-related error due to missing bean declaration

--- src/main/java/br/com/pedrovictor/sistlog/config/AsyncConfig.java
+++ src/main/java/br/com/pedrovictor/sistlog/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package br.com.pedrovictor.sistlog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -8,16 +9,15 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class AsyncConfig {
-
+    @Bean(name = "trackingEventExecutor")
     public Executor trackingEventExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5); // Número mínimo de threads disponíveis
         executor.setMaxPoolSize(10); // Número máximo de threads simultâneas
-        executor.setQueueCapacity(50); // Número máximo de tarefas aguardando execução
+        executor.setQueueCapacity(20); // Número máximo de tarefas aguardando execução
         executor.setThreadNamePrefix("TrackingEventThread-");
         executor.initialize();
 
-        executor.setQueueCapacity(20); // Mantém um número razoável de tarefas em espera
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy()); // Evita descarte de tarefas
         return executor;
     }
