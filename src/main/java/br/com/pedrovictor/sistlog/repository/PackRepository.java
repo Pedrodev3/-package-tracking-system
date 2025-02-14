ee4d007245c6e13e8d7a45f7d921b281aac31090
Pedro Victor Saraiva <pedrosaraivadev@gmail.com>
2025-02-13 21:08:40 -0300

    Feat: Created queryes to bring packages with cancelled status from one year ago - Capture by id -> Focus in perfomance

--- src/main/java/br/com/pedrovictor/sistlog/repository/PackRepository.java
+++ src/main/java/br/com/pedrovictor/sistlog/repository/PackRepository.java
@@ -1,11 +1,15 @@
 package br.com.pedrovictor.sistlog.repository;
 
 import br.com.pedrovictor.sistlog.domain.Pack;
+import br.com.pedrovictor.sistlog.domain.PackStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,4 +34,12 @@ public interface PackRepository extends JpaRepository<Pack, Long> {
             "WHERE (:recipients IS NULL OR p.recipient.recipient IN :recipients) " +
             "AND  (:senders IS NULL OR p.sender.sender IN :senders)")
     List<Pack> findByRecipientsAndOrSenders(List<String> recipients, List<String> senders);
+
+    @Query("SELECT p.id FROM Pack p WHERE p.status = :status AND p.updatedAt < :dateTime")
+    List<Long> findOldCancelledPackageIds(PackStatus status, LocalDateTime dateTime);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Pack p WHERE p.id IN :ids")
+    void deleteBatchByIds(List<Long> ids);
 }
